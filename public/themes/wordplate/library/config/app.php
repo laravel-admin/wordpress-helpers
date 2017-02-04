08352a577d316e892e5e1f09d6bf385a34de6e45
Marco Boom <marco@quest-media.nl>
2017-02-04 10:05:50 +0100

    Refactoring config

--- public/themes/wordplate/library/config/app.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//	Update the location of the uploads. Set to the public storage of laravel
-
-update_option('upload_path', ABSPATH.'../storage');
-update_option('upload_url_path', '/storage');
